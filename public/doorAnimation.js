124ceb259daf70f9f265585a9257ee4d0218b54d
Marino Dzalto <marino.zdalto@fer.hr>
2024-01-18 12:10:19 +0100

    Delay on doorAnimation

--- public/doorAnimation.js
+++ public/doorAnimation.js
@@ -5,5 +5,5 @@ document.addEventListener('DOMContentLoaded', function () {
     setTimeout(function () {
         doorImageLEFT.classList.toggle("doorOpenL");
         doorImageRIGHT.classList.toggle("doorOpenR");
-    }, 500); // Delay the animation by 0.5s after the page loads
+    }, 1000); // Delay the animation by 1s after the page loads
 });
